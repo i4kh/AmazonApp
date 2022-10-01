22142d50e5f4315127f449b2b66f635927e11735
i4kh <haciakh@gmail.com>
2022-10-01 18:33:28 +0400

    Updates

---
+++ precache-manifest.627f96c2fd662e271b0b2df6ff0ee4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32b23c1b6488ec9a9b42",
+    "url": "/i4kh/AmazonApp/static/css/main.189098ab.chunk.css"
+  },
+  {
+    "revision": "32b23c1b6488ec9a9b42",
+    "url": "/i4kh/AmazonApp/static/js/main.3f5aa3ba.chunk.js"
+  },
+  {
+    "revision": "be028af134fc1ffac1e0",
+    "url": "/i4kh/AmazonApp/static/js/runtime~main.6bcc07fe.js"
+  },
+  {
+    "revision": "9268bd057b16d5c7a039",
+    "url": "/i4kh/AmazonApp/static/js/2.4d50f713.chunk.js"
+  },
+  {
+    "revision": "469ae0393c9eca53ea73",
+    "url": "/i4kh/AmazonApp/static/js/3.268c5232.chunk.js"
+  },
+  {
+    "revision": "3596cdcffc9e620349a584195e2075c2",
+    "url": "/i4kh/AmazonApp/index.html"
+  }
+];
